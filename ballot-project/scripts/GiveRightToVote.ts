4b6f5547f7a637b8704f870fb094ae070718abf1
ky <kyria@writeme.com>
2022-11-12 13:20:03 +0800

    commit ballot project

---
+++ ballot-project/scripts/GiveRightToVote.ts
@@ -0,0 +1,39 @@
+import { ethers } from "hardhat";
+import { Ballot, Ballot__factory } from "../typechain-types";
+import * as dotenv from "dotenv"; 
+dotenv.config();
+
+function convertStringArrayToBytes32(array: string[]) {
+  const bytes32Array = [];
+  for (let index = 0; index < array.length; index++) {
+    bytes32Array.push(ethers.utils.formatBytes32String(array[index]));
+  }
+  return bytes32Array;
+}
+
+async function main() {
+    const provider = ethers.getDefaultProvider("goerli");
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "");
+    // const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC ?? "");
+    const signer = wallet.connect(provider);
+    const balanceBN = await signer.getBalance();
+    console.log(`Connected to the account of address ${signer.address}`);
+    console.log(`The balance of this account is ${balanceBN.toString()} Wei`);
+  
+    const args = process.argv;
+    const params = args.slice(2);
+    const contractAddress = params[0];
+    const targetAccount = params[1];
+
+    let ballotContract: Ballot;
+    const ballotFactory = new Ballot__factory(signer);
+    ballotContract = ballotFactory.attach(contractAddress);
+    const tx = await ballotContract.giveRightToVote(targetAccount);
+    const receipt = await tx.wait();
+    console.log(receipt);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
